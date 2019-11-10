885221202526ef9935834fdb3b32b3eb90791c5e
Md Ashikur Rahman <mdashikrah@gmail.com>
2019-11-10 19:48:54 +0600

    XGBoost Updated

--- Rent.py
+++ Rent.py
@@ -7,7 +7,7 @@ from sklearn import linear_model, model_selection
 from sklearn.metrics import explained_variance_score
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("E:/Kaggle/Competition/HouseRent/kc_house_data.csv")
+data = pd.read_csv("E:/Kaggle/Competition/HouseRent/HouseRent/kc_house_data.csv")
 
 features = data.iloc[:, 3:].columns.tolist()
 target = data.iloc[:, 2].name
@@ -25,7 +25,7 @@ data_correlations.loc[data_correlations['Value'].abs().sort_values(ascending=Fal
 
 y = data.loc[:, ['sqft_living', 'grade', target]].sort_values(target, ascending=True).values
 x = np.arange(y.shape[0])
-print(y)
+
 'exec(%matplotlib inline)'
 
 plt.subplot(3, 1, 1)
@@ -54,13 +54,12 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_s
 regr.fit(X_train, y_train)
 regr.score(X_test, y_test)
 
-
 # Let's try XGboost algorithm to see if we can get better results
 xgb = xgboost.XGBRegressor(n_estimators=100, learning_rate=0.08, gamma=0, subsample=0.75,
                            colsample_bytree=1, max_depth=7)
 
-traindf, testdf = train_test_split(X_train, test_size = 0.3)
-xgb.fit(X_train,y_train)
+traindf, testdf = train_test_split(X_train, test_size=0.3)
+xgb.fit(X_train, y_train)
 
 predictions = xgb.predict(X_test)
-print(explained_variance_score(predictions,y_test))
+print(explained_variance_score(predictions, y_test))
